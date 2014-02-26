53c37982860bcc20793a5d04ba3a4ee3ce5dfa10
ilonks@gmail.com <ilonks@gmail.com@7021f86e-6ece-9e73-ef27-15de4da87856>
2014-02-26 03:35:36 +0000

    Added in the heart rate monitor stuff

--- trunk/myAlly/src/com/dbt/myally/MainActivity.java
+++ trunk/myAlly/src/com/dbt/myally/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dbt.myally;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -10,12 +13,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button heartrate_button = (Button) findViewById(R.id.heart_rate);
+		
+		heartrate_button.setOnClickListener(new View.OnClickListener() {			
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(MainActivity.this, HeartRateMonitor.class);
+    			startActivity(i);
+			}
+		});
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
+		//getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
