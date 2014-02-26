a18e260858fec11a4365bc5f3e20dc908175ceb7
ilonks <ilonks@gmail.com>
2014-02-26 03:35:36 +0000

    Added in the heart rate monitor stuff

--- myAlly/gen/com/dbt/myally/R.java
+++ myAlly/gen/com/dbt/myally/R.java
@@ -21,13 +21,22 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int green_icon=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int red_icon=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int heart_rate=0x7f080000;
+        public static final int image=0x7f080004;
+        public static final int layout=0x7f080001;
+        public static final int preview=0x7f080005;
+        public static final int text=0x7f080003;
+        public static final int top=0x7f080002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -35,6 +44,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int default_text=0x7f050003;
         public static final int hello_world=0x7f050002;
     }
     public static final class style {
