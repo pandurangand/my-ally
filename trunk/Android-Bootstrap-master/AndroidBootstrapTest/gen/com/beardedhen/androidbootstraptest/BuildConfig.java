7a601287188aac5c84808d697a7fba60ca331286
ilonks@gmail.com <ilonks@gmail.com@7021f86e-6ece-9e73-ef27-15de4da87856>
2014-03-03 22:00:24 +0000

    Change colour and delete bin files and bootstrap from repo

--- trunk/Android-Bootstrap-master/AndroidBootstrapTest/gen/com/beardedhen/androidbootstraptest/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.beardedhen.androidbootstraptest;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
