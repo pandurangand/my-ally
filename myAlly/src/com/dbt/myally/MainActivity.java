2de22b9a16048dd3ba1523198679263c00b7d21b
mariobadr <mariobadr@gmail.com>
2014-03-03 19:13:11 +0000

    Removed BootStrapping

--- myAlly/src/com/dbt/myally/MainActivity.java
+++ myAlly/src/com/dbt/myally/MainActivity.java
@@ -5,8 +5,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
-import com.beardedhen.androidbootstrap.BootstrapButton;
 
 public class MainActivity extends Activity {
 
@@ -15,9 +15,9 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		BootstrapButton heartrate_button = (BootstrapButton) findViewById(R.id.heart_rate);
-		BootstrapButton start_button = (BootstrapButton) findViewById(R.id.start);
-		BootstrapButton earth_button = (BootstrapButton) findViewById(R.id.earth);
+		Button heartrate_button = (Button) findViewById(R.id.heart_rate);
+		Button start_button = (Button) findViewById(R.id.start);
+		Button earth_button = (Button) findViewById(R.id.earth);
 
 		heartrate_button.setOnClickListener(new View.OnClickListener() {			
 			@Override
