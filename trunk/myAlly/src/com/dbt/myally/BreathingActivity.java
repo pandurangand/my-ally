c75023e8095c6c456b3495ef83e41dc2a07ab801
mariobadr@gmail.com <mariobadr@gmail.com@7021f86e-6ece-9e73-ef27-15de4da87856>
2014-03-05 01:41:24 +0000

    Added Breathe In/Breathe Out text to breathing activity.

--- trunk/myAlly/src/com/dbt/myally/BreathingActivity.java
+++ trunk/myAlly/src/com/dbt/myally/BreathingActivity.java
@@ -3,6 +3,7 @@ package com.dbt.myally;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.PowerManager;
 import android.view.SurfaceView;
 import android.view.animation.Animation;
@@ -11,21 +12,32 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class BreathingActivity extends Activity {
-	private HeartRateDetector hrDetector;
+	private HeartRateDetector _hrDetector;
+	
+	private Handler _handler = null;
+	private TextView _textViewBreath = null;
+	
+	private boolean _breathIn = true;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_breathing);
+		
+		_hrDetector = new HeartRateDetector((PowerManager) getSystemService(Context.POWER_SERVICE), 
+				(SurfaceView) findViewById(R.id.surfaceView));
+		_hrDetector.init();
 
 		ImageView image = (ImageView) findViewById(R.id.balloon);
 		Animation hyperspaceJump = AnimationUtils.loadAnimation(this, R.anim.animation_breathing);
 		image.startAnimation(hyperspaceJump);
 
-		hrDetector = new HeartRateDetector((PowerManager) getSystemService(Context.POWER_SERVICE), (SurfaceView) findViewById(R.id.surfaceView));
-		hrDetector.init();
-
 		
-		final TextView heartRate = (TextView) findViewById(R.id.textViewHR);
+		_textViewBreath = (TextView) findViewById(R.id.textView_breath);
+		_handler = new Handler();
+		
+		//TBD: Find a better way to update heart rate (perhaps with a listener interface?)
+		final TextView heartRate = (TextView) findViewById(R.id.textView_HR);
 		Thread t = new Thread() {
 
 			@Override
@@ -36,7 +48,7 @@ public class BreathingActivity extends Activity {
 						runOnUiThread(new Runnable() {
 							@Override
 							public void run() {
-								heartRate.setText("" + hrDetector.getHeartRate());
+								heartRate.setText("" + _hrDetector.getHeartRate());
 							}
 						});
 					}
@@ -46,7 +58,7 @@ public class BreathingActivity extends Activity {
 		};
 
 		t.start();
-		
+
 	}
 
 	/**
@@ -55,7 +67,9 @@ public class BreathingActivity extends Activity {
 	@Override
 	public void onResume() {
 		super.onResume();
-		hrDetector.resume();
+		_handler.removeCallbacks(_updateTimeTask);
+		_handler.postDelayed(_updateTimeTask, 1);
+		_hrDetector.resume();
 	}
 
 	/**
@@ -64,6 +78,21 @@ public class BreathingActivity extends Activity {
 	@Override
 	public void onPause() {
 		super.onPause();
-		hrDetector.pause();
+		_handler.removeCallbacks(_updateTimeTask);
+		_hrDetector.pause();
 	}
+	
+	private Runnable _updateTimeTask = new Runnable() {
+		   public void run() {
+			   if(_breathIn) { 
+				   _textViewBreath.setText("Breathe In");
+			   } else {
+				   _textViewBreath.setText("Breathe Out");
+				   
+			   }
+			   
+			   _breathIn = !_breathIn;
+		       _handler.postDelayed(this, 3000);
+		   }
+		};
 }
