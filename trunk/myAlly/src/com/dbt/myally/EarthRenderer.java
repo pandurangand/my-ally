7817212cac3cc2d480b1e79300557137ef4f8f6c
ilonks@gmail.com <ilonks@gmail.com@7021f86e-6ece-9e73-ef27-15de4da87856>
2014-03-12 17:21:05 +0000

    Added lat and long stuff, fixed rotation of globe

--- trunk/myAlly/src/com/dbt/myally/EarthRenderer.java
+++ trunk/myAlly/src/com/dbt/myally/EarthRenderer.java
@@ -1,59 +1,207 @@
 package com.dbt.myally;
 
+import java.nio.FloatBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import rajawali.Object3D;
+import rajawali.animation.Animation3D;
+import rajawali.animation.Animation3D.RepeatMode;
+import rajawali.animation.RotateAnimation3D;
 import rajawali.lights.DirectionalLight;
 import rajawali.materials.Material;
 import rajawali.materials.textures.ATexture.TextureException;
 import rajawali.materials.textures.Texture;
+import rajawali.math.Matrix4;
+import rajawali.math.Quaternion;
+import rajawali.math.vector.Vector3;
 import rajawali.primitives.Sphere;
 import rajawali.renderer.RajawaliRenderer;
 import android.content.Context;
 
 public class EarthRenderer extends RajawaliRenderer {
 	public DirectionalLight mLight;
-    public Object3D mSphere;
-    //public Context context;
-    //public Camera camera;
+	public Object3D mSphere;
 
-    public EarthRenderer(Context context) {
-        super(context);
-        //this.context = context;
-        setFrameRate(60);
-    }
+	protected Vector3 mFriction;
+	protected FloatBuffer mVelocityBuffer;
+	protected int mVelocityBufferHandle;
+	protected float mTime;
+	private Animation3D mAnim = null;
+	private static final int baseDuration = 4000;
+	private static final int minDuration = 1000;
+	private Vector3 mDirection = new Vector3();
+	private int mAnimDuration = baseDuration;
+	private Quaternion mDragRotation = new Quaternion();
 
-    public void initScene() {
-    	mLight = new DirectionalLight(1f, 0.2f, -1.0f); // set the direction
-		mLight.setColor(1.0f, 1.0f, 1.0f);
-		mLight.setPower(2);
-		
+	private final float TOUCH_SCALE_FACTOR = 180f / 10000;
+
+	public EarthRenderer(Context context) {
+		super(context);
+		setFrameRate(60);
+		getCurrentScene().setBackgroundColor(0xDAAA90);
+	}
+
+	public void initScene() {
+
+		mLight = new DirectionalLight(0, 0, -1);
+		mLight.setPower(1);
 		getCurrentScene().addLight(mLight);
 
+		Object3D circle;
 		try {
+
+			float latTor = 43.7f;
+			float longTor = 79.4f;
+			float lat = 25.8f;
+			float lon = -80.2f;
+
 			Material material = new Material();
 			material.addTexture(new Texture("earthColors",
-					R.drawable.earthtruecolor_nasa_big));
+					R.drawable.earth_texture));
 			material.setColorInfluence(0);
+			circle = new Sphere(0.01f, 7, 7);
+			Material m = new Material();
+
+			circle.setMaterial(m);
+			circle.getMaterial().setColor(0xff33ff33);
+
+			lat -= 90;
+			float x = (float) (1 * Math.sin(Math.toRadians(lat)) * Math
+					.cos(Math.toRadians(lon)));
+			float z = (float) (-1 * Math.sin(Math.toRadians(lat)) * Math
+					.sin(Math.toRadians(lon)));
+			float y = (float) (1 * Math.cos(Math.toRadians(lat)));
+			circle.setPosition(x, y, z);
 			mSphere = new Sphere(1, 24, 24);
 			mSphere.setMaterial(material);
 			getCurrentScene().addChild(mSphere);
+			getCurrentScene().addChild(circle);
+			;
 		} catch (TextureException e) {
 			e.printStackTrace();
 		}
 
 		getCurrentCamera().setZ(4.2f);
+		Vector3 rotationAxis = new Vector3(.3f, .9f, .15f);
+		rotationAxis.normalize();
+
+		mAnim = new RotateAnimation3D(rotationAxis, 360);
+		mAnim.setDuration(8000);
+		mAnim.setRepeatMode(RepeatMode.INFINITE);
+		mAnim.setTransformable3D(getCurrentCamera());
+
+		registerAnimation(mAnim);
+		mAnim.play();
+	}
+
+	public void down_press() {
+		cancelAnimation();
+		storeRotation();
 	}
 
 	public void onDrawFrame(GL10 glUnused) {
 		super.onDrawFrame(glUnused);
-		mSphere.setRotY(mSphere.getRotY() + 0.2);
 	}
-	
+
 	public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-		//showLoader();
+		// showLoader();
 		super.onSurfaceCreated(gl, config);
-		//hideLoader();
+		// hideLoader();
 	}
+
+	private void storeRotation() {
+		mDragRotation = getCurrentCamera().getOrientation(mDragRotation);
+	}
+
+	public void pinchOrDragFinished() {
+		// mCameraDistance = mRecentCameraDistance;
+		resetAnimation(true);
+		storeRotation();
+	}
+
+	public void drag(double x, double y) {
+		// Rotates a short distance
+		cancelAnimation();
+		mDirection = new Vector3(x, y, 0f);
+		// Determine how far to rotate and in which direction
+
+		float rotAng = magnitudeOfRotation(x, y) * -TOUCH_SCALE_FACTOR;
+		Vector3 axis = perpendicularAxis(x, y);
+
+		// If our objects have an existing rotation, transform
+		// the axis of rotation
+
+		if (mDragRotation != null) {
+			Matrix4 mat = mDragRotation.toRotationMatrix().inverse();
+			axis = axis.multiply(mat);
+		}
+		axis.normalize();
+
+		// Get the new rotation as a quaternion
+
+		Quaternion rot = new Quaternion();
+		rot.fromAngleAxis(axis, rotAng);
+
+		// Apply any existing rotation to it
+
+		if (mDragRotation != null)
+			rot.multiply(mDragRotation);
+
+		getCurrentCamera().setOrientation(rot);
+		storeRotation();
+
+	}
+
+	private void cancelAnimation() {
+		if (mAnim != null) {
+			mAnim.pause();
+		}
+	}
+
+	private void resetAnimation(boolean sameOrOpposite) {
+		// Start by canceling any existing animation
+
+		cancelAnimation();
+
+		// Get our axis of rotation, perpendicular to the swipe
+		// direction
+
+		Vector3 axis = perpendicularAxis(-mDirection.x, mDirection.y);
+		axis.normalize();
+
+		Quaternion q = new Quaternion();
+		q = mSphere.getOrientation(q);
+		Matrix4 mat = q.toRotationMatrix().inverse();
+		axis = axis.multiply(mat);
+		axis.normalize();
+
+		Vector3 rotationAxis = new Vector3(.3f, .9f, .15f);
+		rotationAxis.normalize();
+
+		mAnim = new RotateAnimation3D(rotationAxis, 360);
+		mAnim.setDuration(8000);
+		mAnim.setRepeatMode(RepeatMode.INFINITE);
+		mAnim.setTransformable3D(getCurrentCamera());
+
+		registerAnimation(mAnim);
+		mAnim.play();
+
+	}
+
+	private float magnitudeOfRotation(double x, double y) {
+		return (float) new Vector3(x, y, 0).length();
+	}
+
+	private Vector3 perpendicularAxis(double x, double y) {
+		// Uses a fairly unsophisticated approach to generating
+		// a perpendicular vector
+
+		if (y == 0)
+			return new Vector3(y, -x, 0);
+		else
+			return new Vector3(-y, x, 0);
+	}
+
 }
--- trunk/myAlly/src/com/dbt/myally/LoadModelFragment.java
+++ trunk/myAlly/src/com/dbt/myally/LoadModelFragment.java
@@ -1,8 +1,8 @@
 package com.dbt.myally;
 
-import rajawali.Camera;
 import rajawali.RajawaliFragment;
 import android.annotation.SuppressLint;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
@@ -12,12 +12,22 @@ import android.view.ViewGroup;
 import android.widget.FrameLayout;
 
 @SuppressLint("NewApi")
-public class LoadModelFragment extends RajawaliFragment implements OnTouchListener {
+public class LoadModelFragment extends RajawaliFragment implements
+		OnTouchListener {
 
 	private EarthRenderer myRenderer;
 	private float mPreviousX;
 	private float mPreviousY;
 	private final float TOUCH_SCALE_FACTOR = 180f / 3200;
+	private static final int NONE = 0;
+
+	private static final int DRAG = 1;
+	private static final int ZOOM = 2;
+
+	private int mode = NONE;
+
+	PointF start = new PointF();
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -25,6 +35,7 @@ public class LoadModelFragment extends RajawaliFragment implements OnTouchListen
 		myRenderer.setSurfaceView(mSurfaceView);
 		super.setRenderer(myRenderer);
 		mSurfaceView.setOnTouchListener(this);
+		
 	}
 
 	@Override
@@ -37,25 +48,71 @@ public class LoadModelFragment extends RajawaliFragment implements OnTouchListen
 		return mLayout;
 	}
 
+	/*
+	 * @Override public boolean onTouch(View v, MotionEvent event) { float x =
+	 * event.getX(); float y = event.getY();
+	 * 
+	 * switch (event.getAction()) {
+	 * 
+	 * case MotionEvent.ACTION_MOVE: float dx = x - mPreviousX; float dy = y -
+	 * mPreviousY;
+	 * 
+	 * Camera c = myRenderer.getCurrentCamera();
+	 * 
+	 * float half_width = myRenderer.vpWidth() / 2; float half_height =
+	 * myRenderer.vpHeight() / 2; if (x < half_width) { dx = (float)
+	 * c.getRotation().y + (-mPreviousX + Math.abs(x)) / 100; } else { dx =
+	 * (float) c.getRotation().y + (Math.abs(x) - mPreviousX) / 100; } if (y <
+	 * half_height) { dy = (float) c.getRotation().x + (-mPreviousY +
+	 * Math.abs(y)) / 100; } else { dy = (float) c.getRotation().x +
+	 * (Math.abs(y) - mPreviousY) / 100; } c.setRotation(dy, dx, 0);
+	 * 
+	 * break;
+	 * 
+	 * }
+	 * 
+	 * mPreviousX = x; mPreviousY = y; return true; }
+	 */
+
 	@Override
 	public boolean onTouch(View v, MotionEvent event) {
 		float x = event.getX();
 		float y = event.getY();
-		
-		switch(event.getAction()) {
-		
-		case MotionEvent.ACTION_MOVE:
-			float dx = x - mPreviousX;
-			float dy = y - mPreviousY;
-			
-			Camera c = myRenderer.getCurrentCamera();
-			c.setRotation(c.getRotation().add(dy * TOUCH_SCALE_FACTOR,dx * TOUCH_SCALE_FACTOR,0));
+
+		switch (event.getAction() & MotionEvent.ACTION_MASK) {
+		case MotionEvent.ACTION_DOWN:
+			start.set(event.getX(), event.getY());
+			mode = DRAG;
+			myRenderer.down_press();
 			break;
+		case MotionEvent.ACTION_UP:
+		case MotionEvent.ACTION_POINTER_UP:
+			mode = NONE;
+			myRenderer.pinchOrDragFinished();
+			break;
+		case MotionEvent.ACTION_MOVE:
+			if (mode == DRAG) {
+				float dx = x - start.x;
+				float dy = y - start.y;
+				if (Math.abs(x) > 10f || Math.abs(y) > 10f)
+					myRenderer.drag(dx, -dy);
+			}
+			/*
+			 * Camera c = myRenderer.getCurrentCamera(); if (c.getRotX() == 180)
+			 * { c.setRotation(c.getRotation().add(dy * TOUCH_SCALE_FACTOR, dx *
+			 * TOUCH_SCALE_FACTOR, 0));
+			 * 
+			 * } else { c.setRotation(c.getRotation().add(-dy *
+			 * TOUCH_SCALE_FACTOR, dx * TOUCH_SCALE_FACTOR, 0)); }
+			 */
+			break;
+
 		}
-		
+
 		mPreviousX = x;
 		mPreviousY = y;
-		
+
 		return true;
 	}
+
 }
