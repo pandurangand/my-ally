b47fc69ac7d1bc154b114ffb4cdc3e89befcbaaa
mariobadr <mariobadr@gmail.com>
2014-04-01 13:27:15 +0000

    Added comments to some classes.

--- MyAllyFinal/src/com/ece1778/myally/dbt/Therapy.java
+++ MyAllyFinal/src/com/ece1778/myally/dbt/Therapy.java
@@ -1,5 +1,12 @@
 package com.ece1778.myally.dbt;
 
+/**
+ * Currently doesn't do much, but for the future all DBT activities should
+ * implement this class.
+ * 
+ * @author Mario
+ *
+ */
 public interface Therapy {
 	public String getName();
 }
