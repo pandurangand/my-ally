30af0d3ea0409fe7be3d9bff0bf2f65af7ea5f86
mariobadr@gmail.com <mariobadr@gmail.com@7021f86e-6ece-9e73-ef27-15de4da87856>
2014-04-01 15:04:27 +0000

    Initial Diary Card Implementation

--- trunk/MyAllyFinal/gen/com/ece1778/myally/R.java
+++ trunk/MyAllyFinal/gen/com/ece1778/myally/R.java
@@ -185,40 +185,69 @@ public final class R {
         public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int balloon=0x7f020000;
-        public static final int cloud=0x7f020001;
-        public static final int continue_arrow=0x7f020002;
-        public static final int continue_button=0x7f020003;
-        public static final int generic_bkg=0x7f020004;
-        public static final int homescreen_crisis=0x7f020005;
-        public static final int homescreen_diary=0x7f020006;
-        public static final int homescreen_untouched=0x7f020007;
-        public static final int ic_launcher=0x7f020008;
-        public static final int mind_jar2=0x7f020009;
-        public static final int mind_jar_bkg=0x7f02000a;
-        public static final int thought_diffusion_bkg=0x7f02000b;
+        public static final int anger=0x7f020000;
+        public static final int anxious=0x7f020001;
+        public static final int balloon=0x7f020002;
+        public static final int cloud=0x7f020003;
+        public static final int continue_arrow=0x7f020004;
+        public static final int continue_button=0x7f020005;
+        public static final int diary_card_bkg=0x7f020006;
+        public static final int fear=0x7f020007;
+        public static final int generic_bkg=0x7f020008;
+        public static final int happy=0x7f020009;
+        public static final int homescreen_crisis=0x7f02000a;
+        public static final int homescreen_diary=0x7f02000b;
+        public static final int homescreen_untouched=0x7f02000c;
+        public static final int ic_launcher=0x7f02000d;
+        public static final int mind_jar2=0x7f02000e;
+        public static final int mind_jar_bkg=0x7f02000f;
+        public static final int misery=0x7f020010;
+        public static final int sad=0x7f020011;
+        public static final int seekbar=0x7f020012;
+        public static final int seekbar_bkg=0x7f020013;
+        public static final int seekbar_progress=0x7f020014;
+        public static final int seekbar_thumb=0x7f020015;
+        public static final int shame=0x7f020016;
+        public static final int thought_diffusion_bkg=0x7f020017;
     }
     public static final class id {
-        public static final int FadingQuoteSwitcher=0x7f0b0007;
-        public static final int action_settings=0x7f0b000d;
+        public static final int FadingQuoteSwitcher=0x7f0b0017;
+        public static final int action_settings=0x7f0b001d;
         public static final int balloon=0x7f0b0002;
         public static final int button1=0x7f0b0005;
-        public static final int drawer_layout=0x7f0b0006;
-        public static final int left_drawer=0x7f0b0008;
-        public static final int mindjar=0x7f0b0009;
+        public static final int drawer_layout=0x7f0b0016;
+        public static final int emotion_anger=0x7f0b0008;
+        public static final int emotion_anxious=0x7f0b000a;
+        public static final int emotion_fear=0x7f0b000c;
+        public static final int emotion_happy=0x7f0b000e;
+        public static final int emotion_misery=0x7f0b0010;
+        public static final int emotion_sad=0x7f0b0012;
+        public static final int emotion_shame=0x7f0b0014;
+        public static final int imageView1=0x7f0b0006;
+        public static final int left_drawer=0x7f0b0018;
+        public static final int linearLayout1=0x7f0b0007;
+        public static final int mindjar=0x7f0b0019;
+        public static final int seekBarAnger=0x7f0b0009;
+        public static final int seekBarAnxious=0x7f0b000b;
+        public static final int seekBarFear=0x7f0b000d;
+        public static final int seekBarHappy=0x7f0b000f;
+        public static final int seekBarMisery=0x7f0b0011;
+        public static final int seekBarSad=0x7f0b0013;
+        public static final int seekBarShame=0x7f0b0015;
         public static final int surfaceView=0x7f0b0004;
         public static final int textView_HR=0x7f0b0001;
         public static final int textView_HRInfo=0x7f0b0000;
         public static final int textView_breath=0x7f0b0003;
-        public static final int thought_layout=0x7f0b000a;
-        public static final int thought_text=0x7f0b000b;
-        public static final int thought_view=0x7f0b000c;
+        public static final int thought_layout=0x7f0b001a;
+        public static final int thought_text=0x7f0b001b;
+        public static final int thought_view=0x7f0b001c;
     }
     public static final class layout {
         public static final int activity_breathing=0x7f030000;
-        public static final int activity_main=0x7f030001;
-        public static final int activity_mindjar=0x7f030002;
-        public static final int activity_tdiffusion=0x7f030003;
+        public static final int activity_diarycard=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_mindjar=0x7f030003;
+        public static final int activity_tdiffusion=0x7f030004;
     }
     public static final class menu {
         public static final int main=0x7f0a0000;
@@ -226,13 +255,23 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f080001;
         public static final int app_name=0x7f080000;
+        public static final int cd_anger=0x7f080008;
+        public static final int cd_anxious=0x7f080009;
         public static final int cd_balloon=0x7f080005;
         /**  Generic 
          */
         public static final int cd_continueButton=0x7f080002;
+        /**  Diary Card Activity 
+         */
+        public static final int cd_diarycard_bkg=0x7f080007;
+        public static final int cd_fear=0x7f08000a;
+        public static final int cd_happy=0x7f08000b;
         /**  Mind Jar Activity 
          */
         public static final int cd_mindjar=0x7f080006;
+        public static final int cd_misery=0x7f08000c;
+        public static final int cd_sad=0x7f08000d;
+        public static final int cd_shame=0x7f08000e;
         /**  Thought Diffusion Activity 
          */
         public static final int default_thought=0x7f080003;
---
+++ trunk/MyAllyFinal/src/com/ece1778/myally/dbt/diary/DiaryCardActivity.java
@@ -0,0 +1,16 @@
+package com.ece1778.myally.dbt.diary;
+
+import com.ece1778.myally.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DiaryCardActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_diarycard);
+	}
+
+}
